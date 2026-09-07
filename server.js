e056a4b9d13b6bab737eec2375cb7a4a6d8cbbbe
tsend3919-beep <tsend3919@gmail.com>
2026-09-07 18:43:55 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`MONGOL TEST running on port ${PORT}`);
+});
